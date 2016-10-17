e70c8a43b99d904e8147b2c60eae4909d2ac2e29
Andrea <arhallier@gmail.com>
2016-10-17 06:59:10 +0000

    everything working, need to work on capturing errors and testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  require 'pp'
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
@@ -62,7 +62,23 @@ class MoviesController < ApplicationController
   end
   
   def search_tmdb
-    @movies=Movie.find_in_tmdb(params[:search_terms])
+    @matching_movies=Movie.find_in_tmdb(params[:search_terms])
+    print @matching_movies
+  end
+  
+  def add_tmdb
+    ids = params[:tmdb_movies]
+    if(ids)
+      pp ids
+      pp "andrea"
+      ids.each_key do |id|
+        Movie.create_from_tmdb(id)
+      end
+      flash[:notice] = "Movies successfully added to Rotten Potatoes"
+    else
+      flash[:notice] = "No movies added to Rotten Potatoes"
+    end
+    redirect_to movies_path
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,15 +2,46 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
-  
-# class Movie::InvalidKeyError < StandardError ; end
-  
-#  def self.find_in_tmdb(string)
-#    begin
-#      Tmdb::Movie.find(string)
-#    rescue Tmdb::InvalidApiKeyError
-#        raise Movie::InvalidKeyError, 'Invalid API key'
-#    end
-#  end
 
+class Movie::InvalidKeyError < StandardError ; end
+  
+  def self.create_from_tmdb(id)
+    begin 
+      Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
+      movie = Tmdb::Movie.detail(id)
+      if (movie)
+        Movie.create!({:title => (movie['title']), :rating => (get_rating(Tmdb::Movie.releases(movie['id']))), :description => (movie['overview']), :release_date => (movie['release_date'])})
+      end
+    rescue Tmdb::InvalidApiKeyError
+      raise Movie::InvalidKeyError, 'Invalid API key'
+    end
+  end
+  
+  def Movie.get_rating(releases)
+    countries = releases['countries']
+    rating = "PG"
+    if (countries)
+      countries.each do |country|
+        if (country['iso_3166_1'] == 'US' and country['certification'] != "")
+          rating = country['certification']
+        end
+      end
+    end
+    return rating
+  end
+  
+  def self.find_in_tmdb(string)
+    begin
+      @string = string
+      Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
+      tbdb_movies = Tmdb::Movie.find(string)
+      @returnArray = []
+      tbdb_movies.each do |movie|
+        @returnArray.push(Hash[:id => movie.id, :title => movie.title, :rating => get_rating(Tmdb::Movie.releases(movie.id)), :release_date => movie.release_date])
+      end
+      return @returnArray
+    rescue Tmdb::InvalidApiKeyError
+      raise Movie::InvalidKeyError, 'Invalid API key'
+    end
+  end
 end
